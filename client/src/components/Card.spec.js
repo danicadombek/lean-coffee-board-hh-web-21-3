44cd5d72aef6ba04ef3324e80aa1c811dc3c51ee
Danica Dombek <danica.dombek@gmx.de>
2021-06-02 16:33:54 +0200

    Update card-component with author, refine stories.js and test

--- client/src/components/Card.spec.js
+++ client/src/components/Card.spec.js
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react'
 import Card from './Card'
 
 describe('Card', () => {
-  it('renders its children', () => {
-    render(<Card>Hello world</Card>)
-    expect(screen.getByText('Hello world')).toBeInTheDocument()
+  it('renders text and author', () => {
+    render(<Card text="This is a text" author="John Doe" />)
+    expect(screen.getByText('This is a text', 'John Doe')).toBeInTheDocument()
   })
 })
