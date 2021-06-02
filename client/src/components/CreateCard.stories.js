b7e74bab544e7eb2a23ec36e49ceeb06486abbeb
Danica Dombek <danica.dombek@gmx.de>
2021-06-02 16:45:37 +0200

    Create component CreateCard with js, test and stories

---
+++ client/src/components/CreateCard.stories.js
@@ -0,0 +1,11 @@
+import CreateCard from './CreateCard'
+  export default {
+      title: 'CreateCard',
+      component: CreateCard
+  }
+    
+  const Template = args => <CreateCard {...args} />
+    
+  export const Default = Template.bind({})
+  Default.args = {}
+        
