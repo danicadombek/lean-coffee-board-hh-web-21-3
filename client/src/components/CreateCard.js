c024b5e36d8ae643de15254fb3959304901b4da6
Danica Dombek <danica.dombek@gmx.de>
2021-06-03 15:06:18 +0200

    Style CreateCard.js, make handleSubmit-function and change first input into textarea

--- client/src/components/CreateCard.js
+++ client/src/components/CreateCard.js
@@ -1,26 +1,48 @@
 import styled from 'styled-components/macro'
 import Button from './Button'
 
-export default function CreateCard() {
+export default function CreateCard({ onSubmit }) {
   return (
     <Wrapper>
-      <Button>&lt; back</Button>
-      <form>
+      <ButtonBack>&lt; back</ButtonBack>
+      <form onSubmit={handleSubmit}>
         <label>
           Text
-          <input type="textarea"></input>
+          <textarea name="text" rows="4" cols="20"></textarea>
         </label>
         <label>
           Author
-          <input type="text"></input>
+          <input name="author" type="text"></input>
         </label>
         <Button>Create card</Button>
       </form>
     </Wrapper>
   )
+  function handleSubmit(event) {
+    event.preventDefault()
+    const form = event.target
+    const textarea = form.elements.text
+    const input = form.elements.author
+    onSubmit(textarea.value, input.value)
+  }
 }
 
 const Wrapper = styled.section`
   display: grid;
   gap: 20px;
+
+  form {
+    display: grid;
+    gap: 20px;
+  }
+
+  label {
+    display: grid;
+    gap: 5px;
+  }
+`
+
+const ButtonBack = styled(Button)`
+  width: fit-content;
+  border: 0;
 `
